6fb00c236ea88196ad5a8afd7b60beeda146e836
GitHub Action <action@github.com>
2021-08-27 04:55:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/nnyjk/nandemoplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nnyjk/nandemoplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nnyjk.nandemoplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nnyjk.nandemoplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nnyjk/nandemoplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nnyjk/nandemoplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nnyjk.nandemoplugin.services
 
+import com.github.nnyjk.nandemoplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
